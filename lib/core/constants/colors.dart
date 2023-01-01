642243ba89d35344fd656e79075e5856b3d92f41
Omar-IDOUDAOUD <pcomar.lenovo@gmail.com>
2023-01-01 18:04:46 +0000

    create light theme

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/core/utils/hex_color.dart';
+
+abstract class CstColors {
+  /// constante colors:
+//bleu #0860FD
+  static Color a = HexColor('#0860FD');
+//green #91FFAB
+  static Color b = HexColor('#91FFAB');
+//task priority: high: #FF567E
+  static Color c = HexColor('#FF567E');
+//task priority: medium: #FFCC56
+  static Color d = HexColor('#FFCC56');
+//task priority: low: #56C5FF
+  static Color e = HexColor('#56C5FF');
+//check mark circle color: #57FBC9
+  static Color f = HexColor('#57FBC9');
+// orange color: #F99B6C
+  static Color g = HexColor('#F99B6C');
+//white bleu color: #19A1ED
+  static Color h = HexColor('#19A1ED');
+//deep purple: #512EF6
+  static Color k = HexColor('#512EF6');
+//categories colors:
+  // green  : #91FFAB
+  static Color l = HexColor('#91FFAB');
+  // blue: #ABEEEE
+  static Color j = HexColor('#ABEEEE');
+  // purple: #EEABEA
+  static Color i = HexColor('#EEABEA');
+  // brown: #EBCCA4
+  static Color s = HexColor('#EBCCA4');
+
+  ///ligt theme colors:
+// navigation unselected tab color: #B8B8B8
+  static Color la = HexColor('#B8B8B8');
+// navigation bar under divider color: #E0DFE4
+  static Color lb = HexColor('#E0DFE4');
+// main body grey color: #878181
+  static Color lc = HexColor('#878181');
+// secondary body grey color: #D5D5D5
+  static Color ld = HexColor('#D5D5D5');
+// body backround color: #F9F9F9
+  static Color le = HexColor('#F9F9F9');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:task_manager/core/constants/colors.dart';
 import 'package:task_manager/view/home/home.dart';
 
 void main() {
@@ -13,9 +14,52 @@ class TaskManger extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(),
+        indicatorColor: Colors.black,
+        tabBarTheme: TabBarTheme(
+          unselectedLabelColor: CstColors.la,
+          labelColor: Colors.black,
+          labelStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+            fontFamily: "Poppins",
+          ),
+          unselectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 15,
+            fontFamily: "Poppins",
+          ),
+        ),
+        colorScheme: ColorScheme.light(
+          brightness: Brightness.light,
+          background: CstColors.le,
+          primary: Colors.black,
+          secondary:  CstColors.lc,
+          tertiary: CstColors.ld,
+          surface: CstColors.lb,
+        ),
+        textTheme: const TextTheme(
+          // headlines
+          headline1: TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 8,
+          ),
+          headline2: TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 10,
+          ),
+          headline3: TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 12,
+          ),
+          headline4: TextStyle(
+            fontFamily: "Poppins",
+            fontSize: 13,
+          ),
+       
+        ),
       ),
       home: const Home(),
     );
