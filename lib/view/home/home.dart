660c067ca9bd7f7d6db3d02bee83e5ed27a10ef1
Omar-IDOUDAOUD <pcomar.lenovo@gmail.com>
2023-01-01 19:50:55 +0000

    add safearea property

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -12,51 +12,54 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: TabBar(
-        controller: TabController(length: 3, vsync: this),
-        overlayColor: MaterialStateProperty.all(Colors.transparent),
-        indicatorSize: TabBarIndicatorSize.label,
-        indicatorWeight: 1.5,
-        tabs: const [
-          Tab(
-            child: Text(
-              "Tasks",
+    return SafeArea(
+      bottom: false,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: TabBar(
+          controller: TabController(length: 3, vsync: this),
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorWeight: 1.5,
+          tabs: const [
+            Tab(
+              child: Text(
+                "Tasks",
+              ),
             ),
-          ),
-          Tab(
-            child: Text(
-              "Completed",
+            Tab(
+              child: Text(
+                "Completed",
+              ),
             ),
-          ),
-          Tab(
-            child: Text(
-              "Profile",
+            Tab(
+              child: Text(
+                "Profile",
+              ),
             ),
-          ),
-        ],
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            height: 46,
-            child: ColoredBox(color: Colors.white),
-          ),
-          Divider(
-            color: Get.theme.colorScheme.surface,
-            thickness: 0,
-            height: 1.5,
-          ),
-          Expanded(
-            child: ColoredBox(
-              color: Get.theme.colorScheme.background,
-              child: TasksTab(),
+          ],
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 46,
+              child: ColoredBox(color: Colors.white),
             ),
-          )
-        ],
+            Divider(
+              color: Get.theme.colorScheme.surface,
+              thickness: 0,
+              height: 1.5,
+            ),
+            Expanded(
+              child: ColoredBox(
+                color: Get.theme.colorScheme.background,
+                child: TasksTab(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
