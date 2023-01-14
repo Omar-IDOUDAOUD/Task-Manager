ca37b625fb439fafb80525695d9614f308540b6d
Omar-IDOUDAOUD <pcomar.lenovo@gmail.com>
2023-01-14 18:38:51 +0000

    add toMap and fromMap function to models

--- lib/data/model/cotegoriy.dart
+++ lib/data/model/cotegoriy.dart
@@ -15,4 +15,26 @@ class CategoryModel {
     this.productivityPerCentag,
     this.tasksNumber = 0,
   });
+
+  factory CategoryModel.fromMap(Map<String, dynamic> map) {
+    return CategoryModel(
+      id: map['id'],
+      title: map['title'],
+      color: Color(map['color']),
+      productivityPerCentag: map['productivity_percentage'],
+      tasksNumber: map['tasks_number'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'color': color.value,
+      'productivity_percentage': productivityPerCentag,
+      'tasks_number': tasksNumber,
+    };
+  }
+
+
 }
--- lib/data/model/task.dart
+++ lib/data/model/task.dart
@@ -1,13 +1,13 @@
 class TaskModel {
-  final int id;
-  final DateTime creationDate;
-  final String title;
-  final String? descreption;
-  final TaskPriorities? priority;
-  final DateTime? terminationDate;
-  final DateTime? completeionDate;
-  final bool completed;
-  final int categoryId;
+  int id;
+  DateTime creationDate;
+  String title;
+  String? descreption;
+  TaskPriorities? priority;
+  DateTime? terminationDate;
+  DateTime? completeionDate;
+  bool completed;
+  int categoryId;
 
   TaskModel({
     required this.id,
@@ -16,10 +16,39 @@ class TaskModel {
     this.descreption,
     this.priority,
     this.terminationDate,
-    this.completeionDate, 
-    this.completed = false, 
+    this.completeionDate,
+    this.completed = false,
     required this.categoryId,
   });
+
+  factory TaskModel.fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      id: map['id'],
+      creationDate: map['creation_date'],
+      title: map['title'],
+      descreption: map['descreption'],
+      priority: TaskPriorities.values.elementAt(map['priority'] as int), 
+      terminationDate: map['termination_date'],
+      completeionDate: map['completeion_date'],
+      completed: map['completed'],
+      categoryId: map['category_id'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'creation_date': creationDate,
+      'title': title,
+      'descreption': descreption,
+      'priority': priority?.index,
+      'termination_date': terminationDate,
+      'completeion_date': completeionDate,
+      'completed': completed,
+      'category_id': categoryId,
+    };
+  }
+
 }
 
 enum TaskPriorities { high, medium, low }
