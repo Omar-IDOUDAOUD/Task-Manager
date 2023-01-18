73d61c48f9340369b11bfca23e16a519fa461877
Omar-IDOUDAOUD <pcomar.lenovo@gmail.com>
2023-01-18 13:52:20 +0000

    add toMap and fromMap functions to models add properties to taskModel that initializ whene the model is beign to sent to view:      categoryTitle & categoryColor .

--- lib/data/model/cotegoriy.dart
+++ lib/data/model/cotegoriy.dart
@@ -2,17 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/data/model/task.dart';
 
 class CategoryModel {
-  final int id;
-  final String title;
-  final Color color;
-  double? productivityPerCentag;
-  int tasksNumber ; 
+  int? id;
+  String title;
+  Color color;
+  double? productivityPerCentage;
+  int tasksNumber;
 
   CategoryModel({
-    required this.id,
+    this.id,
     required this.color,
     required this.title,
-    this.productivityPerCentag,
+    this.productivityPerCentage,
     this.tasksNumber = 0,
   });
+
+  factory CategoryModel.fromMap(Map<String, dynamic> map) {
+    return CategoryModel(
+      id: map['id'],
+      title: map['title'],
+      color: Color(int.parse(map['color_code'])),
+      productivityPerCentage: map['productivity_percentage'],
+      tasksNumber: map['tasks_number'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id' : id, 
+      'title': title,
+      'color_code': color.value,
+      'productivity_percentage': productivityPerCentage,
+      'tasks_number': tasksNumber,
+    };
+  }
 }
--- lib/data/model/task.dart
+++ lib/data/model/task.dart
@@ -1,25 +1,64 @@
+import 'package:flutter/material.dart';
+
 class TaskModel {
-  final int id;
-  final DateTime creationDate;
-  final String title;
-  final String? descreption;
-  final TaskPriorities? priority;
-  final DateTime? terminationDate;
-  final DateTime? completeionDate;
-  final bool completed;
-  final int categoryId;
+  int? id;
+  DateTime creationDate;
+  String title;
+  String? description;
+  TaskPriorities? priority;
+  DateTime? terminationDate;
+  DateTime? completionDate;
+  bool completed;
+  int categoryId;
+  /**those properties is initialed whene this model is being to send to view*/
+  String? categoryTitle; 
+  Color? categoryColor;
 
   TaskModel({
-    required this.id,
+    this.id,
     required this.creationDate,
     required this.title,
-    this.descreption,
+    this.description,
     this.priority,
     this.terminationDate,
-    this.completeionDate, 
-    this.completed = false, 
+    this.completionDate,
+    this.completed = false,
     required this.categoryId,
   });
+
+  factory TaskModel.fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      id: map['id'],
+      creationDate: DateTime.parse(map['creation_date']),
+      title: map['title'],
+      description: map['description'],
+      priority: map['priority'] != null
+          ? TaskPriorities.values.elementAt(map['priority'])
+          : null,
+      terminationDate: map['termination_date'] != null
+          ? DateTime.parse(map['termination_date'])
+          : null,
+      completionDate: map['completion_date'] != null
+          ? DateTime.parse(map['completion_date'])
+          : null,
+      completed: map['completed'] == 1,
+      categoryId: map['category_id'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'creation_date': creationDate.toIso8601String(),
+      'title': title,
+      'description': description,
+      'priority': priority?.index,
+      'termination_date': terminationDate?.toIso8601String(),
+      'completion_date': completionDate?.toIso8601String(),
+      'completed': completed == true ? 1 : 0,
+      'category_id': categoryId,
+    };
+  }
 }
 
-enum TaskPriorities { high, medium, low }
+enum TaskPriorities { low, medium, high }
